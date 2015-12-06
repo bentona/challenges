4964f16b04ebc31e5a8f726fd8bd000411067c5c
bentona <benton.anderson@gmail.com>
2015-12-06 14:26:38 -0800

    day 1 part 2

--- advent/day1/day1.scala
+++ advent/day1/day1.scala
@@ -2,15 +2,23 @@ object Parens{
 
   def main(args: Array[String]){
     val input = io.Source.fromFile(args(0)).mkString
-    val floorDiff = input.split("").foldLeft(0) { (total, p) =>
-      val offset = p match {
-        case "(" => 1
-        case ")" => -1
-        case _ => 0
-      }
-      total + offset
+    val intList = input.split("").map (n => n match {
+      case "(" => 1
+      case ")" => -1
+      case _ => 0
+    })
+
+    println(floorMap(intList).indexOf(-1))
+  }
+
+  def total(intList: Array[Int]) : Int  = {
+    intList.sum
+  }
+
+  def floorMap(intList: Array[Int]) : Array[Int] = {
+    intList.foldLeft(Array(0)){ (floors, n) =>
+      floors :+ (n + floors.last)
     }
-    println(floorDiff)
   }
 
 }
