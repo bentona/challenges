939c0595fbc3f1eb7b43899a0f92e8099fec35db
bentona <benton.anderson@gmail.com>
2015-12-06 13:08:47 -0800

    tabs

--- advent/day1/day1.scala
+++ advent/day1/day1.scala
@@ -1,16 +1,17 @@
 object Parens{
 
-	def main(args: Array[String]){
-		val input = io.Source.fromFile(args(0)).mkString
-		val floorDiff = input.split("").foldLeft(0) { (total, p) =>
-			val offset = p match {
-				case "(" => 1
-				case ")" => -1
-				case _ => 0
-			}
-			total + offset
-		}
-		println(floorDiff)
-	}
+  def main(args: Array[String]){
+    val input = io.Source.fromFile(args(0)).mkString
+    val floorDiff = input.split("").foldLeft(0) { (total, p) =>
+      val offset = p match {
+        case "(" => 1
+        case ")" => -1
+        case _ => 0
+      }
+      total + offset
+    }
+    println(floorDiff)
+  }
 
 }
+
