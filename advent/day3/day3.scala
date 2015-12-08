f58a0069d45a418bea7c3c18a92898894c5f2118
bentona <benton.anderson@gmail.com>
2015-12-07 21:08:55 -0800

    day 3 part 2

--- advent/day3/day3.scala
+++ advent/day3/day3.scala
@@ -3,12 +3,20 @@ object DrunkSanta {
   def main(args: Array[String]){
     val directions = io.Source.fromFile(args(0)).mkString.split("")
     val changes = directions.map( x => coordChange(x) )
-    val visits = changes.foldLeft(Array((0,0))){ (visited, move) =>
+    val splitChanges = changes.grouped(2).toList
+    val origSantaChanges = splitChanges.map(x => x(0))
+    val roboSantaChanges = splitChanges.map(x => x(1))
+
+    val allVisits = Array(origSantaChanges, roboSantaChanges).map( changes => visits(changes) )
+    println(allVisits.flatten.distinct.length)
+  }
+
+  def visits(changes: List[(Int,Int)]): Array[(Int, Int)] = {
+    changes.foldLeft( Array((0,0)) ){ (visited, move) =>
       val previous = visited.last
       val next = ( previous._1 + move._1, previous._2 + move._2 )
       visited :+ next
     }
-    println(visits.distinct.length)
   }
 
   def coordChange(instruction: String): (Int, Int) = {
